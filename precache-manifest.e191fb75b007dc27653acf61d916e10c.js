e2ceaa2374afed024cf267c2a4eb20d3c34b0996
Visal Rao <visalrao19@gmail.com>
2019-08-20 21:00:47 +0530

    Updates

---
+++ precache-manifest.e191fb75b007dc27653acf61d916e10c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1ac2224943c50388c296fcb2ea8e68",
+    "url": "/React-Rematch/index.html"
+  },
+  {
+    "revision": "0442e38eb6dda392634a",
+    "url": "/React-Rematch/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7a8166ef3bbeb9188f71",
+    "url": "/React-Rematch/static/css/main.9f80e40c.chunk.css"
+  },
+  {
+    "revision": "0442e38eb6dda392634a",
+    "url": "/React-Rematch/static/js/2.1e364316.chunk.js"
+  },
+  {
+    "revision": "7a8166ef3bbeb9188f71",
+    "url": "/React-Rematch/static/js/main.81c270a2.chunk.js"
+  },
+  {
+    "revision": "7460aab22e4e52a03197",
+    "url": "/React-Rematch/static/js/runtime~main.7d03df6d.js"
+  }
+]);
